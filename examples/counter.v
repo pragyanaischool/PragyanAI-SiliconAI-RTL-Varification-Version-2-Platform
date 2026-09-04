079eb1b94307441fe6db692f8c757681c5d5209c
Sateesh Ambesange ( PragyanAI) <75975970+pragyanaischool@users.noreply.github.com>
2026-09-04 09:59:22 +0530

    Update counter.v

--- examples/counter.v
+++ examples/counter.v
@@ -1,12 +1,19 @@
-module counter(
-    input clk,
-    input reset,
-    output reg [3:0] count
+`timescale 1ns/1ps
+
+module counter (
+    input  wire       clk,
+    input  wire       reset,
+    input  wire       enable,
+    output reg  [3:0] count
 );
-always @(posedge clk) begin
-    if (reset)
-        count <= 0;
-    else
-        count <= count + 1;
-end
+
+    always @(posedge clk) begin
+        if (reset) begin
+            count <= 4'b0000;
+        end
+        else if (enable) begin
+            count <= count + 4'b0001;
+        end
+    end
+
 endmodule
